0974dd1eb80910dd6624dbb7e85bb76f9444aea1
Felipe Henrique <felipehenriqueam@gmail.com>
2020-01-28 22:09:21 -0300

    Desert Life atualizado com os sprits

---
+++ TransporteDeEscravos/Assets/Scripts/Jogo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Jogo : MonoBehaviour
+{
+
+    public void CarregaIntroducao()
+    {
+        SceneManager.LoadScene("Introducao");
+    }
+
+    public void CarregarFase1()
+    {
+        SceneManager.LoadScene("Fase1");
+    }
+
+
+    public void EncerrarJogo()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
